c3271268d9a1835ac52d2219495bb50f0c436a35
Alana Wittman <wittman26@gmail.com>
2025-07-29 11:56:15 +0200

    update

--- src/contexto4/TradeSignatureServicePostImpl.java
+++ src/contexto4/TradeSignatureServicePostImpl.java
@@ -1,14 +1,14 @@
 package com.acelera.fx.digitalsignature.application.service;
 
 import com.acelera.broker.fx.db.domain.dto.ProductDocumentParameters;
+import com.acelera.broker.fx.db.domain.dto.ProductDocumentParametersRequest;
+import com.acelera.broker.fx.db.domain.port.ProductDocumentParametersRepositoryClient;
 import com.acelera.fx.digitalsignature.domain.port.dto.StartSignatureRequestDto;
 import com.acelera.fx.digitalsignature.domain.port.dto.StartSignatureResponseDto;
-import com.acelera.fx.digitalsignature.domain.port.service.ProductDocumentsService;
 import com.acelera.fx.digitalsignature.domain.port.service.TradeSignatureServicePost;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Locale;
@@ -18,24 +18,33 @@ import java.util.Locale;
 @Slf4j
 public class TradeSignatureServicePostImpl implements TradeSignatureServicePost {
 
-    private final ProductDocumentsService productDocumentsService;
+    private final ProductDocumentParametersRepositoryClient productDocumentClient;
 
     @Override
     public Mono<StartSignatureResponseDto> startSignatureWorkflow(String entity, Locale locale, Long originId,
             StartSignatureRequestDto dto) {
 
         // 1. documentos del producto:
-        var documents = productDocumentsService.findProductDocumentType(entity, locale, dto.getProductId());
-
-        Flux<String> resul= documents.map(this::get4676);
-
-        //TODO Implement method
-        return Mono.just(StartSignatureResponseDto.builder().expedientId(originId).build());
+        return productDocumentClient.findProductDocumentParameters(
+                new ProductDocumentParametersRequest(entity, dto.getProductId()))
+                .doOnNext(doc -> generarDocumentos(doc, originId, dto.getProductId()))
+                .then(
+                    Mono.just(StartSignatureResponseDto.builder().expedientId(originId).build())
+                );
     }
 
-    private String get4676(ProductDocumentParameters document) {
-        log.info("Documento: {} - {}", document.getDocumentType(), document.getDocumentalTypeDoc());
+    private String generarDocumentos(ProductDocumentParameters document, Long originId, String productId) {
+        if(document.getIsPrecontractual().equals("Y")) {
+            log.info("Documento TRUE: {} - {} - {}", document.getDocumentType(), document.getDocumentalTypeDoc(), document.getIsPrecontractual());
+        } else {
+            log.info("Documento FALSE: {} - {} - {}", document.getDocumentType(), document.getDocumentalTypeDoc(), document.getIsPrecontractual());
+        }
 
-        return "KD111727202503071120Pzb.pdf";
+        return document.getDocumentType() + originId + productId + ".pdf";
+    }
+
+    private String generarExpediente(Long originId, String productId, String documentName) {
+        log.info("Generar Expediente: {} - {} - Document Name: {}", originId, productId, documentName);
+        return "998547";
     }
 }
